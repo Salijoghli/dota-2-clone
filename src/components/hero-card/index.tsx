f204ed48b0e99bf98e6a6e3af811e1aca970f5ab
Salijoghli <guga.kakhadze12@gmail.com>
2023-10-14 15:32:38 +0400

    feat: added animation on filtering, added text when there is no match on filter. fix:fixed the navbar language select issue

--- src/components/hero-card/index.tsx
+++ src/components/hero-card/index.tsx
@@ -1,12 +1,13 @@
-import { Box, Typography, Stack, Link } from "@mui/material";
+import { Box, Typography, Stack } from "@mui/material";
 import strengthIcon from "assets/images/strength.png";
 import agilityIcon from "assets/images/agility.png";
 import intelligenceIcon from "assets/images/intelligence.png";
 import universalIcon from "assets/images/universal.png";
 import { ShadowBox } from "components/box-shadow";
 import { Dota2Hero } from "utility/hero-types";
-
 import "components/hero-card/style.css";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 export type Size = "small" | "large";
 
@@ -14,6 +15,7 @@ type HeroCardProps = {
   hero: Dota2Hero;
   size?: Size;
 };
+
 export const HeroCard = ({ hero, size = "large" }: HeroCardProps) => {
   const { name, attribute, image } = hero;
 
@@ -27,66 +29,79 @@ export const HeroCard = ({ hero, size = "large" }: HeroCardProps) => {
       : universalIcon;
 
   return (
-    <Box
-      component={Link}
-      href={`/hero/${hero.name.replaceAll(" ", "")}`}
-      className="hero-card"
-      sx={{
+    <motion.div
+      whileHover={{
+        scale: 1.4,
+        zIndex: 11,
+      }}
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+      style={{
         width: size === "small" ? "225px" : "255px",
         height: "130px",
-        position: "relative",
+        boxShadow: "0 8px 24px rgba(0, 0, 0, 0.5)",
         cursor: "pointer",
-        overflow: "hidden",
-        transformOrigin: "left right",
-        transition: "transform 0.2s ease",
-        // boxShadow: "rgba(0, 0, 0, 0.35) 15px 15px 15px 5px",
       }}
     >
-      <Box className="bottom-shadow" display="none" zIndex={-1}>
-        <ShadowBox bottom={0} height="150px" direction="toTop" />
-      </Box>
-      <Box
-        component={"img"}
-        width="100%"
-        height="100%"
-        zIndex={1}
-        src={image}
-        alt="hero image"
-        sx={{
-          objectFit: "cover",
+      <Link
+        to={`/hero/${name.replaceAll(" ", "")}`}
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          padding: "0 2",
         }}
-      />
-      <Stack
-        className="hero-details"
-        flexDirection={"row"}
-        py={1}
-        alignItems="center"
-        position="absolute"
-        gap={"10px"}
-        width="100%"
-        color="white"
-        zIndex={31}
+        className="hero-card"
       >
         <Box
-          component="img"
-          src={getAttributeLogo()}
-          width="30px"
-          height="30px"
-        />
-        <Typography
-          fontWeight={600}
-          variant="h5"
-          textTransform={"uppercase"}
-          fontFamily="serif"
-          letterSpacing={"1px"}
-          fontSize="25px"
+          component={"img"}
+          width="100%"
+          height="100%"
+          zIndex={1}
+          src={image}
+          alt="hero image"
           sx={{
-            textShadow: "1px 1px black",
+            objectFit: "cover",
+          }}
+        />
+        <Stack
+          className="hero-details"
+          flexDirection={"row"}
+          py={1}
+          px={"auto"}
+          alignItems="center"
+          position="absolute"
+          bottom={0}
+          width="100%"
+          gap={1}
+          color="white"
+          sx={{
+            display: "none",
           }}
         >
-          {name}
-        </Typography>
-      </Stack>
-    </Box>
+          <ShadowBox height="200px" direction="toTop" />
+          <Box
+            component="img"
+            src={getAttributeLogo()}
+            width="28px"
+            height="28px"
+            zIndex={12}
+          />
+          <Typography
+            fontWeight={600}
+            variant="h6"
+            textTransform={"uppercase"}
+            fontFamily="sans-serif"
+            fontSize="18px"
+            zIndex={12}
+          >
+            {name}
+          </Typography>
+        </Stack>
+      </Link>
+    </motion.div>
   );
 };
